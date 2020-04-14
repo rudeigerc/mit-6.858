29300f03c98767b146d8f10d2802f11c851f1fe5
rudeigerc <rudeigerc@gmail.com>
2020-04-14 02:51:36 -0400

    exercise 4 and 5 completed by Zheng Huang

--- zookd.c
+++ zookd.c
@@ -100,7 +100,8 @@ static void process_client(int fd)
 {
     static char env[8192];  /* static variables are not on the stack */
     static size_t env_len = 8192;
-    char reqpath[4096];
+    // char reqpath[4096];
+    char reqpath[8192];    // fix overflow
     const char *errmsg;
 
     /* get the request line */
